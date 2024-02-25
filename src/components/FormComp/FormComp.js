3aa9f2962bce339b70a9956def0172a0cf75a048
kotbegemot1 <kk1slorod@gmail.com>
2024-02-25 16:21:42 +0300

    first commit

--- src/components/FormComp/FormComp.js
+++ src/components/FormComp/FormComp.js
@@ -4,15 +4,47 @@ import React, { Component } from 'react';
 import './FormComp.css';
 
 class FormComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+    
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount()");
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate()");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate()");
+  }
+
+  handleChange = (event) => {
+    this.setState(() => ({input: event.target.value}))
+    console.log(this.state)
+  }
+
   render() {
     return (
-      <form>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Ввод:</label>
-    <input type="email" className="form-control form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+      <>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Ввод:</label>
+          <input 
+          type="email" 
+          className="form-control form" 
+          id="exampleInputEmail1" 
+          aria-describedby="emailHelp" 
+          placeholder="Enter email" 
+
+          />
+        </div>
+        <button type="button" className="btn btn-primary">Submit</button>
+      </>
     );
   }
 }
