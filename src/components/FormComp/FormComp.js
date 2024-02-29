ece0e7424493a4cdeb4dced2a2dec8acc019defe
kotbegemot1 <kk1slorod@gmail.com>
2024-02-29 21:07:45 +0300

    add: lesson_2

--- src/components/FormComp/FormComp.js
+++ src/components/FormComp/FormComp.js
@@ -1,43 +1,51 @@
 
 import React, { Component } from 'react';
+import Dogs from '../Dogs/Dogs'
 
 import './FormComp.css';
 
 class FormComp extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+
+    this.state = {
+      value: "",
+      isButtonDisabled: false
+    };
+
+    this.inputRef = React.createRef();
+
     this.time = props.now;
-    console.log(this.time);
-    console.log("constructor");
+
+    // console.log("constructor");
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    console.log('getDerivedStateFromProps');
-    return null;
-  }
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   console.log('getDerivedStateFromProps');
+  //   return null;
+  // }
 
-  componentDidMount() {
-    console.log('componentDidMount');
-  }
+  // componentDidMount() {
+  //   console.log('componentDidMount');
+  // }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate');
+  //   return true;
+  // }
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapshotBeforeUpdate');
-    return null;
-  }
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log('getSnapshotBeforeUpdate');
+  //   return null;
+  // }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('componentDidUpdate');
-  }
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+  //   console.log('componentDidUpdate');
+  // }
 
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-  }
+  // componentWillUnmount() {
+  //   console.log('componentWillUnmount');
+  // }
 
   handleSubmit = (event) => {
     alert('Вы ввели: ' + this.state.value);
@@ -45,31 +53,46 @@ class FormComp extends Component {
   }
 
   handleChange = (event) => {
-    this.setState({value: event.target.value});
-    console.log(this.state);
+    const value = event.target.value;
+    const isButtonDisabled = value.toLowerCase().includes('реакт');
+    this.setState({ value, isButtonDisabled });
+  }
+
+  handleFocusClick = () => {
+    // Устанавливаем фокус в инпут
+    this.inputRef.current.focus();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <p class="h3">{12 < this.time < 24 ? 'Добрый день!' : 'Добрый вечер!'}</p>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Ввод:</label>
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-            className="form-control form"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-
-          />
-        </div>
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <p className="h3">{6 < this.time < 18 ? 'Добрый день!' : 'Добрый вечер!'}</p>
+          <div className="form-group">
+            <label htmlFor="exampleInput">Ввод:</label>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              ref={this.inputRef}
+              className="form-control form"
+              id="exampleInput"
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={this.state.isButtonDisabled}
+            className="btn btn-primary"
+          >button</button>
+        </form>
         <button
-          type="submit"
+          type='button'
+          style={{ marginTop: '10px' }}
+          onClick={this.handleFocusClick}
           className="btn btn-primary"
-        >button</button>
-      </form>
+        >focus</button>
+        <Dogs />
+      </>
     );
   }
 }
